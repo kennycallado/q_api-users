db16d42f75bf87bab2d68b528f61fbf25bd07e7c
kennycallado <kennycallado@hotmail.com>
2023-05-10 13:32:22 +0200

    FEATURE Only attach the database to rocket if the migrations_run config parameter is true

--- src/app/server.rs
+++ src/app/server.rs
@@ -4,17 +4,25 @@ use crate::config::cors;
 use crate::config::database;
 
 use super::modules::routing as modules_routing;
+use super::providers::interfaces::helpers::config_getter::ConfigGetter;
 use super::routing as service_routing;
 
 #[launch]
 pub fn rocket() -> _ {
-    rocket::build()
+    let mut rocket_build = rocket::build();
+
+    // Only attach the database if migrations_run is true
+    if ConfigGetter::get_migrations_run().unwrap_or(false) {
+        rocket_build = rocket_build
+            .attach(database::Db::fairing())
+            .attach(AdHoc::on_ignite(
+                "Diesel Migrations",
+                database::run_migrations,
+            ));
+    }
+
+    rocket_build
+        .attach(cors::Cors)
         .attach(service_routing::router())
         .attach(modules_routing::router())
-        .attach(database::Db::fairing())
-        .attach(AdHoc::on_ignite(
-            "Diesel Migrations",
-            database::run_migrations,
-        ))
-        .attach(cors::Cors)
 }
