39ea8b3077207d3445e0c9570e8a6f59936c4eab
kennycallado <kennycallado@hotmail.com>
2023-05-10 13:27:24 +0200

    Revert "FMT"
    This reverts commit 8ee0689b47d01191a20aa2bc28f5a8189aa0ce2e.

--- src/app/server.rs
+++ src/app/server.rs
@@ -4,25 +4,17 @@ use crate::config::cors;
 use crate::config::database;
 
 use super::modules::routing as modules_routing;
-use super::providers::interfaces::helpers::config_getter::ConfigGetter;
 use super::routing as service_routing;
 
 #[launch]
 pub fn rocket() -> _ {
-    let mut rocket_build = rocket::build();
-
-    if ConfigGetter::get_migrations_run().unwrap_or(false) {
-        println!("Using Rocket Database");
-        rocket_build = rocket_build
-            .attach(AdHoc::on_ignite(
-                "Diesel Migrations",
-                database::run_migrations,
-            ))
-            .attach(database::Db::fairing());
-    }
-
-    rocket_build
+    rocket::build()
         .attach(service_routing::router())
         .attach(modules_routing::router())
+        .attach(database::Db::fairing())
+        .attach(AdHoc::on_ignite(
+            "Diesel Migrations",
+            database::run_migrations,
+        ))
         .attach(cors::Cors)
 }
