8ddf4dfeee5126d2e9ad9281e394e4b52425ad9e
kennycallado <kennycallado@hotmail.com>
2023-05-29 13:30:20 +0200

    ADD

---
+++ src/app/providers/interfaces/answer.rs
@@ -0,0 +1,11 @@
+use serde::{Deserialize, Serialize};
+
+// use crate::app::providers::interfaces::question::PubQuestion;
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct PubAnswer {
+    pub id: i32,
+    pub question_id: i32,
+    pub answer: String,
+}
--- src/app/providers/interfaces/mod.rs
+++ src/app/providers/interfaces/mod.rs
@@ -1,3 +1,4 @@
+pub mod answer;
 pub mod helpers;
 pub mod question;
 pub mod resource;
