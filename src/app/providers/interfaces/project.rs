5687928aa1d7f2e81e17d545e22773bba7e232f8
kennycallado <kennycallado@hotmail.com>
2023-06-06 15:39:54 +0200

    UPDATE Agrega fcm e incluye especializaciones:  - PubFcmToken: init_user()  - PubProject: init_user()

---
+++ src/app/providers/interfaces/project.rs
@@ -0,0 +1,45 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct PubProject {
+    pub id: i32,
+    pub name: String,
+    pub keys: Vec<Option<String>>,
+}
+
+impl PubProject {
+    pub async fn init_user(fetch: &State<Fetch>, project_id: i32, user_id: i32) -> Result<Self, Status> {
+        let robot_token = match Fetch::robot_token().await {
+            Ok(token) => token,
+            Err(_) => return Err(Status::InternalServerError),
+        };
+
+        let project_url = ConfigGetter::get_entity_url("project").unwrap_or("http://localhost:8051/api/v1/project".to_string())
+            + "/"
+            + project_id.to_string().as_str()
+            + "/user/"
+            + user_id.to_string().as_str()
+            + "/new";
+
+        let client = fetch.client.lock().await;
+        let res = client
+            .get(project_url)
+            .header("Accept", "application/json")
+            .header("Authorization", robot_token)
+            .send()
+            .await;
+
+
+        match res {
+            Ok(res) => {
+                if !res.status().is_success() {
+                    return Err(Status::from_code(res.status().as_u16()).unwrap());
+                }
+
+                Ok(res.json::<Self>().await.unwrap())
+            }
+            Err(_) => Err(Status::InternalServerError)
+        }
+    }
+}
