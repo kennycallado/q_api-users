d8b1d77f74a2fe52410cc3c473c8ea706253465b
kennycallado <kennycallado@hotmail.com>
2023-06-19 16:42:44 +0200

    ADD Agrega PubPrjectWithRecords logics needs

--- src/app/providers/models/project.rs
+++ src/app/providers/models/project.rs
@@ -94,3 +94,11 @@ impl PubProject {
     }
 }
 
+#[derive(Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct PubProjectWithRecords {
+    pub id: i32,
+    pub name: String,
+    pub keys: Vec<Option<String>>,
+    pub records: Option<Vec<PubRecord>>,
+}
