e4d9ed29d0562917b5385f8e8aca86672d207083
kennycallado <kennycallado@hotmail.com>
2023-06-19 14:29:31 +0200

    ADD Agrega pubpaper that logics needs

--- src/app/providers/models/paper.rs
+++ src/app/providers/models/paper.rs
@@ -16,6 +16,15 @@ pub struct PubPaperPush {
 
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(crate = "rocket::serde")]
+pub struct PubPaper {
+    pub id: i32,
+    pub project_id: i32,
+    pub resource_id: i32,
+    pub completed: bool,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
 pub struct PubNewPaper {
     pub user_id: i32,
     pub project_id: i32,
