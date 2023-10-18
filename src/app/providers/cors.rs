cce712845111feaf3d9d863ec5d1701e601fec5e
kennycallado <kennycallado@hotmail.com>
2023-10-18 13:22:21 +0200

    UPDATE Cors manage more than one origin

--- src/app/providers/cors.rs
+++ src/app/providers/cors.rs
@@ -15,8 +15,22 @@ impl Fairing for Cors {
         }
     }
 
-    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
-        let origin = ConfigGetter::get_origin_url().unwrap_or("*".to_string());
+    async fn on_response<'r>(&self, request: &'r Request<'_>, response: &mut Response<'r>) {
+        let c_origin = request.headers().get_one("Origin");
+        let origins = ConfigGetter::get_origin_url()
+            .unwrap_or("*".to_string())
+            .to_string();
+
+        let allowed_origins = if origins.contains(',') {
+            origins.split(',').collect::<Vec<&str>>()
+        } else {
+            vec![&*origins]
+        };
+
+        let origin = match c_origin {
+            Some(claimed_origin) if allowed_origins.contains(&claimed_origin) => claimed_origin,
+            _ => "*",
+        };
 
         response.set_header(Header::new("Access-Control-Allow-Origin", origin));
         response.set_header(Header::new(
